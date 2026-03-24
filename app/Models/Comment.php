04186c46f7240cc6ef6155b41979216be660f969
murhaf <www.murhaf2004@gmail.com>
2026-03-25 01:21:45 +0300

    making migration

---
+++ app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'comment',
+        'status',
+        'user_id',
+        'post_id'
+    ];
+
+}
